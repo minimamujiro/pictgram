15123d4e1292760ad496a192bccf574613efab6e
minimamujiro <minimamujiro@yahoo.co.jp>
2022-03-13 22:06:06 +0900

    動作確認済み修正 #3

--- src/main/java/com/example/pictgram/controller/SessionsController.java
+++ src/main/java/com/example/pictgram/controller/SessionsController.java
@@ -5,28 +5,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SessionController {
+public class SessionsController {
 
 	@GetMapping(path = "/login")
 	public String index() {
-		return "session/new";
+		return "sessions/new";
 	}
-	
+
 	@GetMapping(path = "/login-failure")
 	public String loginFailure(Model model) {
 		model.addAttribute("hasMessage", true);
 		model.addAttribute("class", "alert-danger");
 		model.addAttribute("message", "Emailまたはパスワードに誤りがあります。");
-		
-		return "session/new";
+
+		return "sessions/new";
 	}
-	
-	@GetMapping(path = "logout-complete")
+
+	@GetMapping(path = "/logout-complete")
 	public String logoutComplete(Model model) {
 		model.addAttribute("hasMessage", true);
 		model.addAttribute("class", "alert-info");
 		model.addAttribute("message", "ログアウトしました。");
-		
-		return "layout/complete";
+
+		return "layouts/complete";
 	}
 }
--- src/main/java/com/example/pictgram/controller/UsersController.java
+++ src/main/java/com/example/pictgram/controller/UsersController.java
@@ -26,37 +26,38 @@ public class UsersController {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	@GetMapping(path = "/users/new")
 	public String newUser(Model model) {
-	    model.addAttribute("form", new UserForm());
+		model.addAttribute("form", new UserForm());
 		return "users/new";
 	}
-	
+
 	@RequestMapping(value = "/user", method = RequestMethod.POST)
 	public String create(@Validated @ModelAttribute("form") UserForm form, BindingResult result, Model model, RedirectAttributes redirAttrs) {
 		String name = form.getName();
 		String email = form.getEmail();
 		String password = form.getPassword();
 		String passwordConfirmation = form.getPasswordConfirmation();
-		
+
 		if (repository.findByUsername(email) != null) {
 			FieldError fieldError = new FieldError(result.getObjectName(), "email", "その E メールはすでに使用されています。");
 			result.addError(fieldError);
 		}
 		if (result.hasErrors()) {
-			model.addAttribute("hasMessage",true);
-			model.addAttribute("class","alert-danger");
-			model.addAttribute("message","ユーザー登録に失敗しました。");
+			model.addAttribute("hasMessage", true);
+			model.addAttribute("class", "alert-danger");
+			model.addAttribute("message", "ユーザー登録に失敗しました。");
 			return "users/new";
 		}
+
 		User entity = new User(email, name, passwordEncoder.encode(password), Authority.ROLE_USER);
 		repository.saveAndFlush(entity);
 		
 		model.addAttribute("hasMessage", true);
 		model.addAttribute("class", "alert-info");
-		model.addAttribute("message", "ユーザーの登録が完了しました。");
-		
+		model.addAttribute("message", "ユーザー登録が完了しました。");
+
 		return "layouts/complete";
 	}
 }
